e86371f0a5c076332c9e4ccbba75a940e55e10a0
Genny Lou Diaz <55771929+GennyDiaz@users.noreply.github.com>
2023-06-21 13:42:56 +0800

    Initial commit

---
+++ Index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GennyDiaz</title>
+</head>
+<body>
+    <p> Genny Lou Diaz <p>
+    <p> Freelancer | Senior Quality Assurance <p>
+    <p>I'm an experienced Quality Assurance Tester for 3 years until now based on Philippines.
+         I'm graduate degree on BS-IT with Multimedia Design and Animation. I like to explore and find new challenges that will allows
+          me to gained new knowledge while working.<p>
+
+    <p> insert cv here <p>
+    <break>
+    <h2>Education & Experience </h2>
+
+</body>
+</html>
